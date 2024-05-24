b95f62017484a66ff0f3a9ada0e5a0631b46483a
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-05-24 18:07:38 +0300

    Edited banner

--- src/components/Pages/Banner/Banner.tsx
+++ src/components/Pages/Banner/Banner.tsx
@@ -4,7 +4,7 @@ import st from "./Banner.module.css";
 const Banner: FC = () => {
   return (
     <div className={st.Banner}>
-      <video autoPlay muted loop poster="/banner_prev.png">
+      <video autoPlay muted loop>
         <source src="/banner.mp4" type="video/mp4" />
       </video>
       <div className={st.Info}>
