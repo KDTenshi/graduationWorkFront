4e842ec6e5eeaea154191fb338b589c096cb6e2e
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-05-24 19:06:02 +0300

    Edited sertificates

--- src/components/Pages/Sertificates/Sertificates.tsx
+++ src/components/Pages/Sertificates/Sertificates.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import st from "./Sertificates.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Sertificates: FC = () => {
   const sertificates = ["/sertificate_1.jpeg", "/sertificate_2.jpeg", "/sertificate_3.jpeg"];
@@ -10,9 +11,9 @@ const Sertificates: FC = () => {
       <h2>Наши сертификаты</h2>
       <div className={st.Wrap}>
         {sertificates.map((sertificate) => (
-          <div className={st.Image} key={sertificate}>
+          <Link href={sertificate} className={st.Image} key={sertificate} target="_blank">
             <img src={sertificate} alt="Sertificate" loading="lazy" />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
