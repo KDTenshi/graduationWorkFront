aa1bfda67779b511684cb1318fe5b64f6b339482
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-05-27 20:39:41 +0300

    Made little improvements

--- src/components/Pages/BenefitCard/BenefitCard.tsx
+++ src/components/Pages/BenefitCard/BenefitCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import st from "./BenefitCard.module.css";
 import { TBenefit } from "@/Types";
+import Image from "next/image";
 
 interface BenefitCardProps {
   benefit: TBenefit;
@@ -11,6 +12,7 @@ const BenefitCard: FC<BenefitCardProps> = ({ benefit }) => {
     <div className={st.Card}>
       <h3>{benefit.title}</h3>
       <p>{benefit.text}</p>
+      <Image src={benefit.img} alt="Benefit" width={64} height={64} />
     </div>
   );
 };
